a703c7d4e55b44614baa9dafc6f214effbcab0c2
JeronimoTejera <69091990+JeronimoTejera@users.noreply.github.com>
2020-08-14 19:44:21 -0300

    Parte 2 del individual
    Básicamente copié lo que había en "categories" y "category-info", respectivamente.
    Hice algunas adecuaciones leves para adaptarlo a productos.

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,48 @@
+var product = {}
+function showImagesGallery(array){
+
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < array.length; i++){
+        let images = array[i];
+
+        htmlContentToAppend += `
+        <div class="col-lg-3 col-md-4 col-6">
+            <div class="d-block mb-4 h-100">
+                <img class="img-fluid img-thumbnail" src="` + images + `" alt="">
+            </div>
+        </div>
+        `
+
+        document.getElementById("imageesproducto").innerHTML = htmlContentToAppend;
+    }
+}
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            product = resultObj.data;
 
+            let categoryNameHTML  = document.getElementById("productName");
+            let categoryDescriptionHTML = document.getElementById("productDescription");
+            let productCountHTML = document.getElementById("productCount");
+            let productCriteriaHTML = document.getElementById("categoriaaa");
+            let productCoast = document.getElementById("precio");
+            
+            categoryNameHTML.innerHTML = product.name;
+            categoryDescriptionHTML.innerHTML = product.description;
+            productCountHTML.innerHTML = product.soldCount;
+            productCriteriaHTML.innerHTML = product.category;
+            productCoast.innerHTML = product.cost;
+
+            //Muestro las imagenes en forma de galería
+            showImagesGallery(product.images);
+        }
+    });
 });
