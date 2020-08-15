aaac56b0c3a999104a6994ef43d12920b1d718ed
JeronimoTejera <69091990+JeronimoTejera@users.noreply.github.com>
2020-08-15 16:31:41 -0300

    Parte 3-segunda parte
    Validacion del login mediante una funcion en JScript

--- js/login.js
+++ js/login.js
@@ -1,3 +1,13 @@
+var usuario= document.getElementById("usuario");
+var password= document.getElementById("password");
+
+function validar(){
+    if(usuario === '' || password===''){
+        return false
+    }else{
+        return true
+    }
+}
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
