07b9dcc2db7d0255121316d37703c866ac1ec730
JeronimoTejera <69091990+JeronimoTejera@users.noreply.github.com>
2020-08-25 18:50:26 -0300

    Parte grupal entrega 2
    Introduje una funcion para guardar los datos del login.
    Introduje un funcion para mostrar el usuario en el inicio (tuve que ponerla directamente como <script> en el html del login porque no supe referenciarla en el mismo)

--- js/login.js
+++ js/login.js
@@ -5,8 +5,17 @@ function validar(){
     if(usuario === '' || password===''){
         return false
     }else{
-        return true
+        guardarDatos();
+        return true;
     }
+}
+
+function guardarDatos(){
+    var user = usuario.value;
+    var pass = password.value;
+    sessionStorage.setItem("user", user);
+    sessionStorage.setItem('pass', pass);
+
 }
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
